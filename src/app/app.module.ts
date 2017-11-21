c2166797d373155f615cdea245218fdd3f7d1267
lyukhymchak <l.yukhymchak@gmail.com>
2017-11-21 12:05:54 +0200

    Added SignIn form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SigninFormComponent } from './components/signin-form/signin-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/signin-form/signin-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'signin-form',
+  templateUrl: './signin-form.component.html',
+  styleUrls: ['./signin-form.component.css']
+})
+export class SigninFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
