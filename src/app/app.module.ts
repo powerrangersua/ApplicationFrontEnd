300d84ac93a304ce4729be23146a359dfe8df4dd
lyukhymchak <l.yukhymchak@gmail.com>
2017-11-21 11:58:13 +0200

    Added SignUp form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
